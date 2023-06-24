739bf516263c7eb6d9648705020ae5c2d6861f19
Naman Sharma <namansharm149@gmail.com>
2023-06-24 22:39:19 +0545

    newchange

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const input=document.querySelector('.in1')
-const btn=document.querySelector('.btn')
-
+const input=document.querySelector('#in1')
+const btn=document.querySelector('#btn')
+const box=document.querySelector('#box')
 input.addEventListener('input',(e)=>{
-    
+    if(e.target){
+        let p=document.createElement('p')
+        p.innerHTML='<b></b>'
+    }
 })
