d88cdee60dbcdba7cdbe61b295c71b8e30d91f01
Naman Sharma <namansharm149@gmail.com>
2023-06-24 23:08:00 +0545

    lastcommitofday

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const input=document.querySelector('#in1')
 const btn=document.querySelector('#btn')
 const box=document.querySelector('#box')
 input.addEventListener('input',(e)=>{
-    if(e.target){
+    if(e){
         let p=document.createElement('p')
-        p.innerHTML='<b></b>'
+        p.innerHTML=`<b>${e.target.value}</b>`
+        document.body.append(p)
     }
+    console.log(e.target.value.toString().split(''))
+})
+btn.addEventListener('click',()=>{
+    console.log('btn clicked')
 })
