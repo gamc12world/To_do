4318b43d2324f89af3dea94e77bea44379eacd05
Naman Sharma <namansharm149@gmail.com>
2023-06-25 21:14:34 +0545

    fgdfg

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const input = document.querySelector("#in1");
 const btn = document.querySelector("#btn");
 const box = document.querySelector("#box");
+let arr = [];
 input.addEventListener("input", (e) => {
   let x = e.target.value;
-  if (e.target) {
-    if (
-      btn.addEventListener("click", (e) => {
-        if (e.target) {
-          box.innerHTML = `<b>${x}</b>`;
-          document.body.append(box);
-        }
-      })
-    ) {
+  arr[0]= x;
+});
+
+btn.addEventListener("click", (e) => {
+  arr.forEach((ele) => {
+    let p = document.createElement("li");
+    p.innerHTML = `<li>${ele}</li>`;
+    box.appendChild(p)
+    document.body.append(box);
+  });
+});
+const but=document.createElement('button')
+but.innerHTML='<p>nice button</p>'
+document.body.append(but)
+but.addEventListener('click',(e)=>{
+    if(box.length>0){}
+    else{
+        console.log('nice one')
     }
-  }
-});
-btn.addEventListener("click", () => {
-  console.log("btn clicked");
-});
+})
